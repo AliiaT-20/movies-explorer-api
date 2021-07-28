91836110e0758bdb352e027e5a23232f8c46b071
Aliia.T <troitskayaalia@yandex.ru>
2021-07-29 01:19:18 +0300

    added cors

--- app.js
+++ app.js
@@ -20,15 +20,15 @@ const allowedCors = [
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    const { method } = req;
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-    const requestHeaders = req.headers['access-control-request-headers'];
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-    }
   }
   next();
 });
