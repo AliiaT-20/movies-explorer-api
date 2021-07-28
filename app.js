a5ca06b55d303468ea3dcdd688e679eb54bbc1c3
Aliia.T <troitskayaalia@yandex.ru>
2021-07-29 00:18:02 +0300

    added headers

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   }
 
   next();
