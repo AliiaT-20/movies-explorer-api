cfe93e4733b4e19ae2592b3f1ac7e9ee4b65b4a8
Aliia.T <troitskayaalia@yandex.ru>
2021-07-29 01:41:42 +0300

    added cors

--- app.js
+++ app.js
@@ -12,24 +12,9 @@ const limiter = require('./middlewares/limiter');
 const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 const app = express();
 
-const allowedCors = [
-  'https://aliiat.diplom.nomoredomains.club',
-  'http://aliiat.diplom.nomoredomains.club',
-  'http://localhost:3000',
-];
-
 app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    const { method } = req;
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-    const requestHeaders = req.headers['Access-Control-Request-Headers'];
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-    }
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
