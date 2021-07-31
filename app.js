d0f0ab3fe71321335093f1ca673a221df758c33c
Aliia.T <troitskayaalia@yandex.ru>
2021-07-31 17:45:25 +0300

    edit cors

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ const app = express();
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
@@ -29,6 +26,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Max-Age', 86400);
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
   }
   next();
 });
